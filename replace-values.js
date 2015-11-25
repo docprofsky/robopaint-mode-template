8a60dfa62bdae45ab51b744d0dfa2d24fe6556ae
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-11-25 11:22:17 -0700

    Add list of fields to change when making a new mode

---
+++ replace-values.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "_i18n/template.en-US.json": {
+    "author": /docprofsky/g,
+    "info-name": "info.name",
+    "info-use": "info.use",
+    "info-detail": "info.detail"
+  },
+
+  "package.json": {
+    "author": /docprofsky/g,
+    "name": /template(?!_)/g
+  },
+
+  "template.js": {
+    "name": /template/g
+  },
+
+  "template.ps.js": {
+    "name": /template/g
+  }
+}
