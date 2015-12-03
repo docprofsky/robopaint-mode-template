b65d21b3d264267e97b0b1922012ccaab05122a5
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-12-03 10:29:22 -0700

    Update for config for the new mode-maker 5000p

--- replace-values.js
+++ replace-values.js
@@ -1,21 +1,43 @@
 module.exports = {
-  "_i18n/template.en-US.json": {
-    "author": /docprofsky/g,
-    "info-name": "info.name",
-    "info-use": "info.use",
-    "info-detail": "info.detail"
-  },
+  "replace": {
+    "_i18n/template.en-US.json": {
+      "author": /docprofsky/g,
+      "info-name": "info.name",
+      "info-use": "info.use",
+      "info-detail": "info.detail"
+    },
 
-  "package.json": {
-    "author": /docprofsky/g,
-    "name": /template(?!_)/g
-  },
+    "package.json": {
+      "author": /docprofsky/g,
+      "name": /template(?!_)/g
+    },
 
-  "template.js": {
-    "name": /template/g
-  },
+    "template.js": {
+      "name": /template/g
+    },
 
-  "template.ps.js": {
-    "name": /template/g
+    "template.ps.js": {
+      "name": /template/g
+    },
+
+    "template.html": {
+      "name": /template/g
+    },
+
+    "template.css": {
+      "name": /template/g
+    }
+  },
+  "files": {
+    "rename": [
+      "_i18n/template.en-US.json",
+      "template.css",
+      "template.html",
+      "template.js",
+      "template.ps.js"
+    ],
+    "delete": [
+      ".git"
+    ]
   }
 }
